004b3decb01b582f91ff52e434fe1057f11dde33
Raghav Bali <raghavbali96@yahoo.com>
2024-01-06 11:52:22 +0530

    Update App.tsx and style.css

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
       <div className='box'>
-        <Typography variant="h3" component="div" gutterBottom color="#09111b" position="absolute" style={{ textAlign: 'center', display: 'flex', justifyContent: 'center', paddingTop: '5%', paddingLeft: '5%', width: '100%', height: '100%', zIndex: 1, textShadow: '0px -4px 4px rgba(19, 17, 27, 1)' }}>
+        <Typography variant="h3" component="div" gutterBottom color="#09111b" position="absolute" style={{ textAlign: 'center', display: 'flex', justifyContent: 'center', paddingTop: '5%', width: '100%', height: '100%', zIndex: 1, textShadow: '0px -4px 4px rgba(19, 17, 27, 1)' }}>
           Hello World
         </Typography>
         <span></span>
