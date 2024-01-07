5892d7436fa72c5c8ad2da01cdc9168a87f4278d
Raghav Bali <raghavbali96@yahoo.com>
2024-01-07 19:51:55 +0530

    Add LandingPage component and update App component

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,15 +1,13 @@
 import { useState, useEffect } from 'react';
 import './style.css';
 import { Box, Typography } from '@mui/material';
+import LandingPage from './Components/LandingPage';
 
 function App() {
 
   return (
       <div className='box'>
-        <Typography variant="h3" component="div" gutterBottom color="#09111b" position="absolute" style={{ textAlign: 'center', display: 'flex', justifyContent: 'center', paddingTop: '5%', width: '100%', height: '100%', zIndex: 1, textShadow: '0px -4px 4px rgba(19, 17, 27, 1)' }}>
-          Hello World
-        </Typography>
-        <span></span>
+        <LandingPage />
       </div>
   );
 }
