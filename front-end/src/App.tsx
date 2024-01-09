ce99c6cbeeafbc6744a092c84aa5e066566b73f1
Raghav Bali <raghavbali96@yahoo.com>
2024-01-10 00:40:25 +0530

    Refactor App component and add MUI theme

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,16 +1,37 @@
-import { useState, useEffect } from 'react';
-import './style.css';
-import { Box, Typography } from '@mui/material';
-import LandingPage from './Components/LandingPage';
-import Navbar from './Components/Navbar';
+import "./style.css";
+import LandingPage from "./Components/LandingPage";
+import createTheme from "@mui/material/styles/createTheme";
+import { ThemeProvider } from "@mui/material/styles";
+
+// Or Create your Own theme:
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#09111b",
+    },
+  },
+  components: {
+    MuiTooltip: {
+      styleOverrides: {
+        tooltip: {
+          color: "whitesmoke",
+          backgroundColor: "#09111b",
+        },
+        arrow: {
+          color: "#09111b",
+        },
+      },
+    },
+  },
+});
 
 function App() {
-
   return (
-      <div className='box'>
-        <Navbar />
+    <ThemeProvider theme={theme}>
+      <div className="box">
         <LandingPage />
       </div>
+    </ThemeProvider>
   );
 }
 
