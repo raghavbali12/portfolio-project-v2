aa5a904974db680e14ad22993e5179170a1572f6
Raghav Bali <raghavbali96@yahoo.com>
2024-04-15 21:53:38 -0700

    Refactor BackgroundPage component and add PictureBox component, and update BackgroundText.json and style.css

--- front-end/src/Components/Background/BackgroundPage.tsx
+++ front-end/src/Components/Background/BackgroundPage.tsx
@@ -2,6 +2,7 @@ import { Box, Button, Fade } from "@mui/material";
 import Xarrow, { Xwrapper } from "react-xarrows";
 import PictureBox from "./PictureBox";
 import childhood from "../../Content/Images/childhood-pic.jpg";
+import india from "../../Content/Images/india-pic.jpeg";
 import text from "../../Content/Text/BackgroundText.json";
 import { useEffect } from "react";
 
@@ -20,12 +21,41 @@ function BackgroundPage() {
   return (
     <div className="background-page">
       <Fade in={true} timeout={2000}>
-        <div>
-          <PictureBox
-            picture={childhood}
-            cardPreview={text.childhood.preview}
-            cardContent={text.childhood.content}
-          />
+        <div style={{ display: "flex" }}>
+          <Xwrapper>
+            <div
+              style={{
+                flex: 1,
+                paddingLeft: "3%",
+                paddingTop: "3%",
+              }}
+            >
+              <div id="childhood">
+                <PictureBox
+                  picture={childhood}
+                  cardPreview={text.childhood.preview}
+                  cardContent={text.childhood.content}
+                />
+              </div>
+            </div>
+            <div
+              style={{
+                flex: 1,
+                paddingLeft: "50%",
+                paddingTop: "20%",
+              }}
+            >
+              <div id="india">
+                <PictureBox
+                  picture={india}
+                  cardPreview={text.india.preview}
+                  cardContent={text.india.content}
+                />
+              </div>
+            </div>
+            {/* TODO: Fix so that arrows do not move when expanding cards */}
+            <Xarrow start={"childhood"} end={"india"} />
+          </Xwrapper>
         </div>
       </Fade>
     </div>
